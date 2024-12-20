7916508d1424971d5b62eb1494579247627f639f
Investor26 <166392494+Investor26@users.noreply.github.com>
2024-12-20 15:29:46 +0400

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+import datetime 
+
+def main(): 
+  print("Hello, World!")
+
+ # Get the current time and print it 
+  current _ time = datetime.datetime.now() 
+  print("Current time:" , current _ time) 
+
+if __ name __ == " __ main __ ":
+    main()
