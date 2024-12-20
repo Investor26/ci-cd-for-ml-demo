a3d1d30972e295991d9d5c44d3d5b73e139449ad
Investor26 <166392494+Investor26@users.noreply.github.com>
2024-12-20 15:31:19 +0400

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -4,8 +4,8 @@ def main():
   print("Hello, World!")
 
  # Get the current time and print it 
-  current _ time = datetime.datetime.now() 
-  print("Current time:" , current _ time) 
+  current_time = datetime.datetime.now() 
+  print("Current time:", current_time) 
 
 if __ name __ == " __ main __ ":
     main()
